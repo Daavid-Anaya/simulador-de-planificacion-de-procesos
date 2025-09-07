fa9c46d23a5723f0738af37b78807f078af530db
David Anaya <daavidanaya@gmail.com>
2025-09-07 09:35:14 -0600

    feat: Implementar la clase FCFS (First Come, First Served) para la planificación de procesos

---
+++ src/main/java/com/mycompany/scheduler/FCFS.java
@@ -0,0 +1,35 @@
+package com.mycompany.scheduler;
+
+import java.util.Comparator;
+
+import com.mycompany.model.Proceso;
+import com.mycompany.view.gui.VentanaPrincipal;
+import com.mycompany.view.manejoDeTablas.AgregarATabla;
+
+public class FCFS {
+
+    public void ejecutar() {
+        int tiempoActual = 0;
+
+        // Ordenar por tiempo de llegada
+        VentanaPrincipal.listaProcesos.sort(Comparator.comparingInt(Proceso::getTiempoLlegada));
+
+        for (Proceso p : VentanaPrincipal.listaProcesos) {
+            if (tiempoActual < p.getTiempoLlegada()) {
+                tiempoActual = p.getTiempoLlegada(); // CPU
+            }
+
+            p.setTiempoInicio(tiempoActual);
+            tiempoActual += p.getTiempoRafaga();
+            p.setTiempoFin(tiempoActual);
+
+            // Calcular métricas
+            p.setTiempoRetorno(p.getTiempoFin() - p.getTiempoLlegada());
+            p.setTiempoEspera(p.getTiempoRetorno() - p.getTiempoRafaga());
+            p.setTiempoRespuesta(p.getTiempoEspera());
+            p.setTasaDesperdicio((float)p.getTiempoEspera() / (float)p.getTiempoRetorno());
+            p.setTasaPenalizacion((float)p.getTiempoRetorno() / (float)p.getTiempoRafaga());
+        }
+        new AgregarATabla();
+    }
+}
